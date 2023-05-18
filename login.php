ee3f5f9a4aaee8d897e7a209c40d0aab6de1374f
Daniel <east.danielteddyllandaa@gmail.com>
2023-05-17 18:29:46 -0700

    Update login.php
    login

--- login.php
+++ login.php
@@ -5,10 +5,11 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>LOG IN</title>
+    <link rel="stylesheet" href="assets/css/bootstrap.min.css">
 </head>
 <body>
-    <main>
-        
+    <main class="container">
+        asdfas
     </main>
     
 </body>
